9fd0af841af94d5889dedc32fa0c702ce0c735d6
Estevao Viana <estevaomagnoviana@gmail.com>
2026-05-18 00:16:34 -0300

    arquivos do para site saraivanet

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const yearElement = document.getElementById("current-year");
+    if (yearElement) {
+        yearElement.textContent = new Date().getFullYear();
+    }
+});
